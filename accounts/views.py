f9c607c5cc3fe7d4ffef298745b5bb008553d4ca
Adam Cloyd Esparcia <adamcloydesparcia@gmail.com>
2025-10-06 23:08:32 +0800

    feature(boacms): add some styles and displays in the dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,8 @@ import re
 
 # Create your views here.
 def index(request):
+    if request.user.is_authenticated:
+        return redirect('dashboard')
     return render(request, 'accounts/index.html')
 
 def register(request):
