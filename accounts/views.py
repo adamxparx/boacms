c216fbb1902fd2fdb3081420b724ca88bb66a592
Adam Cloyd H. Esparcia <adamcloydesparcia@gmail.com>
2025-10-13 15:16:57 +0800

    fix(accounts): clean unnecessary imports, comments, and quotes

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
 from django.contrib import messages
 from .forms import CustomUserCreationForm
-import re
 
 # Create your views here.
 def index(request):
@@ -16,19 +15,17 @@ def register(request):
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
-            user.is_active = True  # Optional: you can require email verification before activation
+            user.is_active = True 
             user.save()
             messages.success(request, 'Your account has been created successfully!')
-            return redirect('login')  # or wherever your login page is
+            return redirect('login') 
     else:
         form = CustomUserCreationForm()
     return render(request, 'accounts/register.html', {'form': form})
 
 @login_required
 def dashboard(request):
-    # Retrieve user information to display on the dashboard
     context = {
         'user': request.user,
-        # Add other context variables as needed
     }
     return render(request, 'accounts/dashboard.html', context)
