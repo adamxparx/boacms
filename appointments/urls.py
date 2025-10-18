0bef3d8a54fe989994a5e96c66d9b57fa2238bde
Adam Cloyd H. Esparcia <adamcloydesparcia@gmail.com>
2025-10-18 20:52:26 +0800

    feature(appointments): add appointment pages, views, and links

--- appointments/urls.py
+++ appointments/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('certification/', views.certification, name='certification'),
+    path('barangay_clearance/', views.barangay_clearance, name='barangay_clearance'),
+    path('certificate_of_indigency/', views.certificate_of_indigency, name='indigency_certificate'),
+    path('community_tax_certificate/', views.comm_tax_certificate, name='comm_tax_cert'),
+    path('solo_parent_certificate/', views.solo_parent_certificate, name='solo_parent_cert'),
 ] 
--- appointments/views.py
+++ appointments/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required   
 
-def certification(request):
-    return render(request, 'appointments/certification.html')
+@login_required
+def barangay_clearance(request):
+    return render(request, 'appointments/barangay_clearance.html')
+
+def certificate_of_indigency(request):
+    return render(request, 'appointments/certificate_of_indigency.html')
+
+def comm_tax_certificate(request):
+    return render(request, 'appointments/comm_tax_certificate.html')
+
+def solo_parent_certificate(request):
+    return render(request, 'appointments/solo_parent_certificate.html')
