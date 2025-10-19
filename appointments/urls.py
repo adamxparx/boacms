20ac1b2cddc14578e7c52315e27652297a9c159a
Adam Cloyd H. Esparcia <adamcloydesparcia@gmail.com>
2025-10-20 00:23:16 +0800

    feature(appointments): create appointments page for both resident and barangay staff, create views and urls for each

--- appointments/urls.py
+++ appointments/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('appointments/', views.appointments, name='appointments'),
+    path('staff_appointments/', views.staff_appointments, name='staff_appointments'),
     path('barangay_clearance/', views.barangay_clearance, name='barangay_clearance'),
     path('certificate_of_indigency/', views.certificate_of_indigency, name='indigency_certificate'),
     path('community_tax_certificate/', views.comm_tax_certificate, name='comm_tax_cert'),
