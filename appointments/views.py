f8a2126fbd82d02739bcd16550d8579c92e0ade1
LAB23_USER ON WS16 <L23X15W16@LabHCI.net>
2025-10-20 16:24:39 +0800

    feature(appointments): create files for certficate of indigency, models, forms, UI, views

--- appointments/views.py
+++ appointments/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .forms import AppointmentForm, BarangayClearanceForm
+from .forms import AppointmentForm, BarangayClearanceForm, CertificateOfIndigencyForm, CommunityTaxCertificateForm
 from .models import Appointment, CertificationType
 
 @login_required
@@ -34,11 +34,67 @@ def barangay_clearance(request):
 
     return render(request, 'appointments/barangay_clearance.html', context)
 
+@login_required
 def certificate_of_indigency(request):
-    return render(request, 'appointments/certificate_of_indigency.html')
+    if request.method == 'POST':
+        appointment_form = AppointmentForm(request.POST)
+        clearance_form = CertificateOfIndigencyForm(request.POST)
 
+        if appointment_form.is_valid() and clearance_form.is_valid():
+            cert_type, created = CertificationType.objects.get_or_create(name="Certificate of Indigency")
+
+            appointment = appointment_form.save(commit=False)
+            appointment.user = request.user
+            appointment.certificate_type = cert_type
+            appointment.save()
+
+            clearance = clearance_form.save(commit=False)
+            clearance.appointment = appointment
+            clearance.save()
+
+            return redirect('appointments')
+        
+    else:
+        appointment_form = AppointmentForm()
+        clearance_form = CertificateOfIndigencyForm()
+        
+        context = {
+            'appointment_form': appointment_form,
+            'clearance_form': clearance_form,
+        }
+
+    return render(request, 'appointments/certificate_of_indigency.html', context)
+
+@login_required
 def comm_tax_certificate(request):
-    return render(request, 'appointments/comm_tax_certificate.html')
+    if request.method == 'POST':
+        appointment_form = AppointmentForm(request.POST)
+        clearance_form = CommunityTaxCertificateForm(request.POST)
+
+        if appointment_form.is_valid() and clearance_form.is_valid():
+            cert_type, created = CertificationType.objects.get_or_create(name="Community Tax Certificate")
+
+            appointment = appointment_form.save(commit=False)
+            appointment.user = request.user
+            appointment.certificate_type = cert_type
+            appointment.save()
+
+            clearance = clearance_form.save(commit=False)
+            clearance.appointment = appointment
+            clearance.save()
+
+            return redirect('appointments')
+        
+    else:
+        appointment_form = AppointmentForm()
+        clearance_form = CommunityTaxCertificateForm()
+        
+        context = {
+            'appointment_form': appointment_form,
+            'clearance_form': clearance_form,
+        }
+
+    return render(request, 'appointments/comm_tax_certificate.html', context)
 
 def solo_parent_certificate(request):
     return render(request, 'appointments/solo_parent_certificate.html')
