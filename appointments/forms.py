4423013117a6ed926c1b09796b4658f5008846da
Adam Cloyd H. Esparcia <adamcloydesparcia@gmail.com>
2025-11-06 21:30:51 +0800

    update(appointments): add server side date and time validation, add 15-minute buffer time for slots

--- appointments/forms.py
+++ appointments/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import Appointment
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+from datetime import time, timedelta
 
 class AppointmentForm(forms.ModelForm):
     class Meta:
@@ -9,3 +12,54 @@ class AppointmentForm(forms.ModelForm):
             'preferred_date': forms.DateInput(attrs={'type': 'date'}),
             'preferred_time': forms.TimeInput(attrs={'type': 'time'}),
         }
+
+    def clean_preferred_date(self):
+        preferred_date = self.cleaned_data.get('preferred_date')
+
+        if preferred_date and preferred_date < timezone.now().date():
+            raise ValidationError("You cannot book a date that is in the past.")
+        
+        return preferred_date
+    
+    def clean_preferred_time(self):
+        preferred_time = self.cleaned_data.get('preferred_time')
+
+        open_time = time(9, 0)
+        close_time = time(16, 30)
+
+        if preferred_time and not (open_time <= preferred_time <= close_time):
+            raise ValidationError("Appointments are only available between 9:00 AM and 4:30 PM")
+        
+        return preferred_time
+    
+    def clean(self):
+        super().clean()
+
+        preferred_date = self.cleaned_data.get('preferred_date')
+        preferred_time = self.cleaned_data.get('preferred_time')
+
+        if preferred_date and preferred_time:
+            buffer_minutes =  15
+
+            try:
+                start_datetime = timezone.make_aware(
+                    timezone.datetime.combine(preferred_date, preferred_time)
+                )
+            except ValueError:
+                raise ValidationError("Invalid date or time.")
+            
+            slot_start = start_datetime - timedelta(minutes=buffer_minutes)
+            slot_end = start_datetime + timedelta(minutes=buffer_minutes)
+
+            conflicting_appointments = Appointment.objects.filter(
+                preferred_date = preferred_date,
+                preferred_time__range = (slot_start.time(), slot_end.time())
+            )
+
+            if self.instance and self.instance.pk:
+                conflicting_appointments = conflicting_appointments.exclude(pk=self.instance.pk)
+
+            if conflicting_appointments.exists():
+                raise ValidationError("This slot is too close to an existing appointment. Please choose a different time.")
+            
+        return self.cleaned_data
